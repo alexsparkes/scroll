62ae4f4fd29523edab381a28386cbc424265770d
alexsparkes <alexsparkes@gmail.com>
2025-02-08 11:28:51 +0000

    fix(navbar): equal spacing

--- src/components/BottomNavbar.tsx
+++ src/components/BottomNavbar.tsx
@@ -10,7 +10,7 @@ import { NavLink } from "react-router-dom";
 export default function BottomNav() {
   return (
     <nav className="h-[75px] fixed bottom-0 left-0 w-full bg-black">
-      <div className="h-full max-w-md mx-auto flex justify-around py-3 items-center">
+      <div className="h-full max-w-md mx-auto flex justify-between items-center px-8 py-3">
         <NavLink
           to="/"
           className={({ isActive }) =>
@@ -24,6 +24,7 @@ export default function BottomNav() {
           </div>
           <span className="text-xs mt-1">Home</span>
         </NavLink>
+
         <NavLink
           to="/discover"
           className={({ isActive }) =>
@@ -37,6 +38,7 @@ export default function BottomNav() {
           </div>
           <span className="text-xs mt-1">Discover</span>
         </NavLink>
+
         <NavLink
           to="/search"
           className={({ isActive }) =>
@@ -50,6 +52,7 @@ export default function BottomNav() {
           </div>
           <span className="text-xs mt-1">Search</span>
         </NavLink>
+
         <NavLink
           to="/saved"
           className={({ isActive }) =>
@@ -63,6 +66,7 @@ export default function BottomNav() {
           </div>
           <span className="text-xs mt-1">Saved</span>
         </NavLink>
+
         <NavLink
           to="/more"
           className={({ isActive }) =>
